731aa8e04fa01a5aaa3bdaa4e39ee2ff60c54baa
Edy <edymola@gmail.com>
2018-10-21 17:01:41 +0200

    Change version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyflarum',
-      version='0.1',
+      version='0.2',
       description='A flarum apy for python',
       url='https://github.com/edymola/pyflarum',
       author='Edymola',
